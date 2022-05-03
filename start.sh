57579c90843c5dc088fdd561ce838f22d92cfa1b
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-05-03 09:08:41 +0300

    run Jekyll with proper parameters to re-load users.json

--- start.sh
+++ start.sh
@@ -1,1 +1,9 @@
-bundle exec jekyll serve --future --incremental --safe --strict_front_matter --limit_posts 15 --open-url --port=4002
+# otherwise Jekyll doesn't re-load users.json
+rm -fr _site
+
+bundle exec jekyll serve --future --incremental --safe --strict_front_matter \
+  --host=0.0.0.0 --port=4002 \
+  --livereload --livereload-port=40002 \
+  --watch \
+  --limit_posts 150000 \
+  --open-url

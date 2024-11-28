4f544ccdcbb9b283c5a6c0acf1924d89c2dda5ac
Andrei Solntsev <andrei.solntsev@gmail.com>
2024-11-28 17:39:18 +0200

    make start script faster: remove only one file instead of whole site

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
 # otherwise Jekyll doesn't re-load users.json
-rm -fr _site
+rm -fr _site/users.html
 
 bundle exec jekyll serve --future --incremental --safe --strict_front_matter \
   --host=0.0.0.0 --port=4002 \
   --livereload --livereload-port=40002 \
   --watch \
-  --limit_posts 150000 \
   --open-url

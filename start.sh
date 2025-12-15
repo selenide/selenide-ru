c9cae7603115613669e882ee252121f4f92d6af5
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-12-15 15:22:07 +0200

    release Selenide 7.13.0

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 # otherwise Jekyll doesn't re-load users.json
 rm -fr _site/users.html
 
+rvm use 2.7
+
 bundle exec jekyll serve --future --incremental --safe --strict_front_matter \
   --host=0.0.0.0 --port=4002 \
   --livereload --livereload-port=40002 \
